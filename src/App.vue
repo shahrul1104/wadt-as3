1550e0bcdf6775922fa987e3d7f4b9539b2762c3
shahrul1104 <20ftt1449@student.pb.edu.bn>
2022-04-14 11:28:01 +0800

    change the css

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,11 @@ import HelloWorld from './components/HelloWorld.vue'
         </div>
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Application for</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Backend Developer</dd>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">web development</dd>
         </div>
         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Email address</dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">margotfoster@example.com</dd>
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">20FTT1449@student.pb.edu.bn</dd>
         </div>
         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="text-sm font-medium text-gray-500">Salary expectation</dt>
@@ -38,22 +38,6 @@ import HelloWorld from './components/HelloWorld.vue'
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
             <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
               <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                <div class="w-0 flex-1 flex items-center">
-                  <PaperClipIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span class="ml-2 flex-1 w-0 truncate"> resume_back_end_developer.pdf </span>
-                </div>
-                <div class="ml-4 flex-shrink-0">
-                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
-                </div>
-              </li>
-              <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                <div class="w-0 flex-1 flex items-center">
-                  <PaperClipIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                  <span class="ml-2 flex-1 w-0 truncate"> coverletter_back_end_developer.pdf </span>
-                </div>
-                <div class="ml-4 flex-shrink-0">
-                  <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Download </a>
-                </div>
               </li>
             </ul>
           </dd>
@@ -64,13 +48,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+img {
+  width: 100px;
+  height: 100px;
+  text-align: center;
+  
+
 }
 </style>
 
